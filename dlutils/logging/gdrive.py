40181ace2150c2daafd32365688be5478c9cdc93
txuna <b7712983@naver.com>
2023-10-04 17:19:59 +0900

    2차 코드리뷰(코드정리)

--- dlutils/logging/gdrive.py
+++ dlutils/logging/gdrive.py
@@ -4,15 +4,14 @@ import os
 import platform
 from datetime import datetime
 
-from gerror import Error
-from gerror import ERROR_STRING
-
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
+
+from gerror import ERROR_STRING, Error
 
 SCOPES = [
     "https://www.googleapis.com/auth/drive.metadata.readonly",
